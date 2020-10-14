541623e93831af98b64835f5764c7c385dc3b4fe
swissChili <swisschili@fastmail.com>
2020-10-14 11:01:15 -0700

    Use JDK 11 compatible switch to support Oracle JVM

--- client/src/main/java/sh/swisschili/chat/client/ThemeFactory.java
+++ client/src/main/java/sh/swisschili/chat/client/ThemeFactory.java
@@ -4,14 +4,21 @@ import com.github.weisj.darklaf.theme.*;
 
 public class ThemeFactory {
     public static Theme byName(String name) {
-        return switch (name) {
-            case "Darcula" -> new DarculaTheme();
-            case "Solarized Light" -> new SolarizedLightTheme();
-            case "Solarized Dark" -> new SolarizedDarkTheme();
-            case "One Dark" -> new OneDarkTheme();
-            case "High Contrast Light" -> new HighContrastLightTheme();
-            case "High Contrast Dark" -> new HighContrastDarkTheme();
-            default -> new IntelliJTheme();
-        };
+        switch (name) {
+            case "Darcula":
+                return new DarculaTheme();
+            case "Solarized Light":
+                return new SolarizedLightTheme();
+            case "Solarized Dark":
+                return new SolarizedDarkTheme();
+            case "One Dark":
+                return new OneDarkTheme();
+            case "High Contrast Light":
+                return new HighContrastLightTheme();
+            case "High Contrast Dark":
+                return new HighContrastDarkTheme();
+            default:
+                return new IntelliJTheme();
+        }
     }
 }

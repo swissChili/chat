a125413febfa8f1c553b81ac30118fcc65e4e4ad
swissChili <swisschili@fastmail.com>
2020-10-30 11:21:56 -0700

    Remove string block to work in Java 8

--- util/src/main/java/sh/swisschili/chat/util/Constants.java
+++ util/src/main/java/sh/swisschili/chat/util/Constants.java
@@ -3,20 +3,20 @@ package sh.swisschili.chat.util;
 public class Constants {
     public static int DEFAULT_SERVER_PORT = 4776;
 
-    public static String LICENSE_HEADER = """
-            Decentralized chat software
-            Copyright (C) 2020  swissChili
+    public static String LICENSE_HEADER =
+            "Decentralized chat software\n" +
+            "Copyright (C) 2020  swissChili\n\n" +
+
+            "This program is free software: you can redistribute it and/or modify\n" +
+            "it under the terms of the GNU Affero General Public License as published by\n" +
+            "the Free Software Foundation, either version 3 of the License, or\n" +
+            "(at your option) any later version.\n\n" +
+
+            "This program is distributed in the hope that it will be useful,\n" +
+            "but WITHOUT ANY WARRANTY; without even the implied warranty of \n" +
+            "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" +
+            "GNU Affero General Public License for more details.\n\n" +
                         
-            This program is free software: you can redistribute it and/or modify
-            it under the terms of the GNU Affero General Public License as published by
-            the Free Software Foundation, either version 3 of the License, or
-            (at your option) any later version.
-                        
-            This program is distributed in the hope that it will be useful,
-            but WITHOUT ANY WARRANTY; without even the implied warranty of
-            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-            GNU Affero General Public License for more details.
-                        
-            You should have received a copy of the GNU Affero General Public License
-            along with this program.  If not, see <https://www.gnu.org/licenses/>.""";
+            "You should have received a copy of the GNU Affero General Public License\n" +
+            "along with this program.  If not, see <https://www.gnu.org/licenses/>.";
 }

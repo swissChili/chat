4c072c70c6d1245616d37f7725e416ea1ae1e340
swissChili <swisschili@fastmail.com>
2020-12-23 19:01:25 -0800

    Fix CI, fonts

--- client/src/main/java/sh/swisschili/chat/client/MessageCell.java
+++ client/src/main/java/sh/swisschili/chat/client/MessageCell.java
@@ -27,8 +27,11 @@ import org.slf4j.LoggerFactory;
 import sh.swisschili.chat.util.ChatProtos.Message;
 
 import javax.swing.*;
+import javax.swing.plaf.FontUIResource;
+import javax.swing.text.StyleContext;
 import java.awt.*;
 import java.util.Date;
+import java.util.Locale;
 
 public class MessageCell implements ListCellRenderer<Message> {
     private JPanel rootPanel;
@@ -110,7 +113,10 @@ public class MessageCell implements ListCellRenderer<Message> {
                 resultName = currentFont.getName();
             }
         }
-        return new Font(resultName, style >= 0 ? style : currentFont.getStyle(), size >= 0 ? size : currentFont.getSize());
+        Font font = new Font(resultName, style >= 0 ? style : currentFont.getStyle(), size >= 0 ? size : currentFont.getSize());
+        boolean isMac = System.getProperty("os.name", "").toLowerCase(Locale.ENGLISH).startsWith("mac");
+        Font fontWithFallback = isMac ? new Font(font.getFamily(), font.getStyle(), font.getSize()) : new StyleContext().getFont(font.getFamily(), font.getStyle(), font.getSize());
+        return fontWithFallback instanceof FontUIResource ? fontWithFallback : new FontUIResource(fontWithFallback);
     }
 
     /**
